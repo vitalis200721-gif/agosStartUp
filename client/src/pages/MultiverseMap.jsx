d5338ba322127424e60614bfb5a7e2de35eb3a03
vitalis200721-gif/type29.git <vitalis200721@gmail>
2026-03-01 10:03:16 +0200

    fix: removed broken crazygames embeds that threw 403s and replaced them with 50+ fully functional HTML5 proxy games

--- client/src/pages/MultiverseMap.jsx
+++ client/src/pages/MultiverseMap.jsx
@@ -164,7 +164,7 @@ export default function MultiverseMap() {
           {/* Iframe Container */}
           <div className="flex-1 w-full bg-black relative">
             <iframe 
-              src={activeGame.embedUrl || `https://www.crazygames.com/embed/${activeGame.embedSlug || activeGame.id}`} 
+              src={activeGame.embedSlug || activeGame.embedUrl} 
               style={{ width: '100%', height: '100%', border: 'none' }} 
               sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-presentation allow-pointer-lock allow-downloads"
               allow="gamepad; microphone; camera; display-capture; midi; xr-spatial-tracking; fullscreen; autoplay; payment; pointer-lock"
